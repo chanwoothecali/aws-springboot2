aac94e9256cad60a5698a1484737bc58a2e358cd
ChanWoo <jungcali94@gmail.com>
2021-12-20 18:08:50 +0900

    Posts 등록 로직 추가

--- src/main/java/com/cali/app/springboot/service/posts/PostsService.java
+++ src/main/java/com/cali/app/springboot/service/posts/PostsService.java
@@ -1,4 +1,19 @@
 package com.cali.app.springboot.service.posts;
 
+import com.cali.app.springboot.domain.post.PostsRepository;
+import com.cali.app.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
 public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
 }
