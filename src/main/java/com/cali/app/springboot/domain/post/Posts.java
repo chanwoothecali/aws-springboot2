5a5971f7e30aa66a50461edc57e7ce98c8355e61
ChanWoo <jungcali94@gmail.com>
2021-12-20 15:07:11 +0900

    Posts dao 추가 및 Test 추가

---
+++ src/main/java/com/cali/app/springboot/domain/post/Posts.java
@@ -0,0 +1,32 @@
+package com.cali.app.springboot.domain.post;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Posts {
+
+    @Id // PK 필드
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // PK 생성규칙
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
