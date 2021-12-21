beed197803f34f44ee9b5b910fefcf5d1344aaad
ChanWoo <jungcali94@gmail.com>
2021-12-21 14:50:52 +0900

    JPA Auditing BaseTimeEntity

--- src/main/java/com/cali/app/springboot/domain/post/Posts.java
+++ src/main/java/com/cali/app/springboot/domain/post/Posts.java
@@ -1,5 +1,6 @@
 package com.cali.app.springboot.domain.post;
 
+import com.cali.app.springboot.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,7 @@ import javax.persistence.*;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id // PK 필드
     @GeneratedValue(strategy = GenerationType.IDENTITY) // PK 생성규칙
