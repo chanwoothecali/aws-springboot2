903b485d0af25d61e7c8edd68e1f8e5a6e6bed0a
jungcali <jungcali94@gmail.com>
2021-12-25 10:48:16 +0900

    세션값 받는 부분 리팩토링(임시)

---
+++ src/main/java/com/cali/app/springboot/config/auth/LoginUserArgumentResolver.java
@@ -0,0 +1,6 @@
+package com.cali.app.springboot.config.auth;
+
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+
+public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver {
+}
