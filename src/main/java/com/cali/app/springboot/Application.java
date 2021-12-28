9ba783c930c2e345ae538c52c16cb9bb49612a7f
ChanWoo <jungcali94@gmail.com>
2021-12-28 17:35:45 +0900

    test용 security config 등록

--- src/main/java/com/cali/app/springboot/Application.java
+++ src/main/java/com/cali/app/springboot/Application.java
@@ -2,9 +2,8 @@ package com.cali.app.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
+//@EnableJpaAuditing
 @SpringBootApplication
 public class Application {
 
---
+++ src/main/java/com/cali/app/springboot/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.cali.app.springboot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
